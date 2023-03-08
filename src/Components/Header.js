4dc3a247fe2f887582f4f4601faf7e67ebc70495
Dayaldonadkar <Dayalpython@gmail.com>
2023-03-08 14:40:15 +0530

    responsive navbar Initialized

---
+++ src/Components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+const Header = () => {
+  return (
+    <div className="flex md:flex-row flex-col md:justify-around  md:items-center bg-[#131921] text-white py-4 gap-y-5 px-5">
+      <img
+        className="md:w-28 w-20 items-center mt-3 object-contain mr-5"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="flex align-center rounded-3xl h-8">
+        <input className="border-none md:px-56 w-full" type="text" />
+        <div className="bg-[#cd9042]">
+          <SearchIcon fontSize="large" className="px-1" />
+        </div>
+      </div>
+
+      <div className="flex justify-between md:space-x-20">
+        <div className="flex flex-col">
+          <span className="text-xs text-gray-100">Hello</span>
+          <span className="text-lg">Sign In</span>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="text-xs text-gray-100">Returns</span>
+          <span className="text-lg">Orders</span>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="text-xs text-gray-100">Your</span>
+          <span className="text-lg">Prime</span>
+        </div>
+
+        <div className="flex lg:space-x-2  items-center">
+          <ShoppingBasketIcon />
+          <span>0</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOpkxfEHXd1wP7qNS5xeZdUfUoHnsm9DY",
+  authDomain: "challenge-5fc88.firebaseapp.com",
+  projectId: "challenge-5fc88",
+  storageBucket: "challenge-5fc88.appspot.com",
+  messagingSenderId: "390856018037",
+  appId: "1:390856018037:web:291f958db8adf5e7b9103c",
+  measurementId: "G-K39XXZFGGH",
+};
